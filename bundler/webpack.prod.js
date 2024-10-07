da5f1a7abfe901dd41b1d618256d5cf02e76fe6a
Marcos Jova <marcosjova3@gmail.com>
2024-10-07 19:52:33 +0530

    update

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,24 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { merge } = require('webpack-merge');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+    mode: 'production',
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    performance: {
+        hints: 'warning', // Show a warning for asset sizes exceeding the limit
+        maxEntrypointSize: 244000, // Maximum entrypoint size in bytes
+        maxAssetSize: 244000, // Maximum asset size in bytes
+        // Optionally, you can add a custom message:
+        // assetFilter: assetFilename => {
+        //     return assetFilename.endsWith('.js') || assetFilename.endsWith('.css'); // Limit to specific file types
+        // }
+    },
+    // Optional: Uncomment the following lines to include the Webpack Bundle Analyzer
+    // plugins: [
+    //     ...,
+    //     new BundleAnalyzerPlugin() // Uncomment this if you install the plugin
+    // ],
+});
